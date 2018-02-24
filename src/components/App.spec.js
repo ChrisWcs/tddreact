581734bbddec1181113037a305d54c1df6dcc54d
Christian <ChrisWcs@users.noreply.github.com>
2018-02-24 10:54:34 -0700

    Add test to App.spec.js

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -12,5 +12,13 @@ describe('App tests', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.children("MyInput").exists()).toBe(true);
   });
+
+  it('App contains myInput', () => {
+    const wrapper = shallow(<App />);
+    const event = {target: {value: "temp"}};
+    wrapper.children('MyInput').children('input').simulate('change', event);
+    wrapper.children('MyInput').children('button').simulate('click', {});
+    expect(wrapper.state().value).toBe("temp");
+  });
 });
 
